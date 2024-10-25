dc77fbacf93735449ad2cc4c82c11f057acb6941
isharanilangani <98374135+isharanilangani@users.noreply.github.com>
2024-10-25 22:32:20 +0530

    feat:add book controller

--- online-bookstore-back/controllers/bookController.js
+++ online-bookstore-back/controllers/bookController.js
@@ -18,3 +18,35 @@ exports.getBooks = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+// Update Book
+exports.updateBook = async (req, res) => {
+  try {
+    const { id } = req.params; // Get the ID from the URL parameters
+    const updatedBook = await Book.findByIdAndUpdate(id, req.body, { new: true });
+
+    if (!updatedBook) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    res.status(200).json(updatedBook);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Delete Book
+exports.deleteBook = async (req, res) => {
+  try {
+    const { id } = req.params; // Get the ID from the URL parameters
+    const deletedBook = await Book.findByIdAndDelete(id);
+
+    if (!deletedBook) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    res.status(200).json({ message: "Book deleted successfully", deletedBook });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
