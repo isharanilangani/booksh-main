281c303ecc6fc442e164fb50d9067147ae2f301d
isharanilangani <98374135+isharanilangani@users.noreply.github.com>
2024-10-25 22:36:30 +0530

    feat:add book route

--- online-bookstore-back/routes/bookRoutes.js
+++ online-bookstore-back/routes/bookRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { addBook, getBooks } = require('../controllers/bookController');
+const { addBook, getBooks, updateBook, deleteBook } = require('../controllers/bookController');
 const router = express.Router();
 
 router.post('/book', addBook);
 router.get('/books', getBooks);
+router.put("/book/:id", updateBook); 
+router.delete("/book/:id", deleteBook); 
 
 module.exports = router;
