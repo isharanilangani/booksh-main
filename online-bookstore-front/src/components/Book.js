8550c8a89721194c670080877fd8aa6e9d8febd6
Ishara Dabarera <98374135+isharanilangani@users.noreply.github.com>
2024-10-26 09:53:15 +0530

    Update Book.js

--- online-bookstore-front/src/components/Book.js
+++ online-bookstore-front/src/components/Book.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react"; 
-import { useNavigate } from "react-router-dom"; // Import useNavigate
+import { useNavigate } from "react-router-dom"; 
 import axios from "axios";
 
 const BooksPage = () => {
